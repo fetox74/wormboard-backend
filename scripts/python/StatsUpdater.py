06564e5ebefcb4f4a127c6087fda50b8d141cc3e
Oliver Dittrich <odi@conleos.com>
2017-09-28 13:41:49 +0200

    starting to adapt stats updater to new data model (still missing the logic for hourly stats)

--- scripts/python/StatsUpdater.py
+++ scripts/python/StatsUpdater.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
+import gzip
+import json
 import re
-import urllib2, gzip, json
+import urllib2
 from StringIO import StringIO
 from multiprocessing.pool import ThreadPool
-from itertools import islice
+
 import psycopg2
+from itertools import islice
 
 
 def partition(data, SIZE=100):
@@ -84,9 +87,16 @@ def getFinalHitCorp(attackers):
 
 def getIskLossForCorp(corp):
     if corp in lossDict:
-        return lossDict[corp]
+        return lossDict[corp]["iskLost"]
     else:
         return 0.0
+
+
+def getLossesForCorp(corp):
+    if corp in lossDict:
+        return lossDict[corp]["losses"]
+    else:
+        return 0
 
 
 def getAttackersOfCorp(attackers, corporation):
@@ -97,6 +107,10 @@ def getAttackersOfCorp(attackers, corporation):
     return result
 
 
+def getHourOfKill(datetime):
+    return "00"
+
+
 def updateMasterDict(killmailCREST, killmailZKB):
     if killmailZKB:
         finalHitCorp = getFinalHitCorp(killmailCREST["attackers"])
@@ -105,17 +119,20 @@ def updateMasterDict(killmailCREST, killmailZKB):
             attackersOfFinalHitCorp = getAttackersOfCorp(killmailCREST["attackers"], finalHitCorp)
             if finalHitCorp in masterDict:
                 masterDict[finalHitCorp]["kills"] = masterDict[finalHitCorp]["kills"] + 1
-                masterDict[finalHitCorp]["isk"] = masterDict[finalHitCorp]["isk"] + killmailZKB["zkb"]["totalValue"]
+                masterDict[finalHitCorp]["iskWon"] = masterDict[finalHitCorp]["iskWon"] + killmailZKB["zkb"]["totalValue"]
                 masterDict[finalHitCorp]["active"] = masterDict[finalHitCorp]["active"] | attackersOfFinalHitCorp
-                masterDict[finalHitCorp]["sumonkills"] = masterDict[finalHitCorp]["sumonkills"] + len(attackersOfFinalHitCorp)
+                masterDict[finalHitCorp]["sumOnKills"] = masterDict[finalHitCorp]["sumOnKills"] + len(attackersOfFinalHitCorp)
             else:
-                masterDict[finalHitCorp] = {"kills": 1, "isk": killmailZKB["zkb"]["totalValue"], "active": attackersOfFinalHitCorp, "sumonkills": len(attackersOfFinalHitCorp)}
+                masterDict[finalHitCorp] = {"kills": 1, "iskWon": killmailZKB["zkb"]["totalValue"], "active": attackersOfFinalHitCorp,
+                                            "sumOnKills": len(attackersOfFinalHitCorp)}
         if victimCorp in lossDict:
-            lossDict[victimCorp] = lossDict[victimCorp] + killmailZKB["zkb"]["totalValue"]
+            lossDict[victimCorp]["losses"] = lossDict[victimCorp]["losses"] + 1
+            lossDict[victimCorp]["iskLost"] = lossDict[victimCorp]["iskLost"] + killmailZKB["zkb"]["totalValue"]
         else:
-            lossDict[victimCorp] = killmailZKB["zkb"]["totalValue"]
+            lossDict[victimCorp] = {"losses": 1, "iskLost": killmailZKB["zkb"]["totalValue"]}
     else:
         print "kill id " + killmailCREST["killID_str"] + " seems not to exist on zKillboard.."
+
 
 def queryAggregateAlreadyInDB(cur, date, corp):
     cur.execute('SELECT * FROM "zwhAggregate" WHERE "date" = ' + date + ' AND "corporation" = ' "'" + corp + "'")
@@ -127,7 +144,18 @@ def queryAggregateAlreadyInDB(cur, date, corp):
 
 def updateDB(cur, date):
     for key, value in masterDict.items():
-        cur.execute('INSERT INTO "zwhAggregate" ("date", "corporation", "kills", "isk", "active", "numactive", "netisk", "sumonkills") VALUES (%i, %s, %i, %f, %s, %i, %f, %i)' % (int(date), "'" + key.replace("'", "''") + "'", value["kills"], value["isk"], "'" + ",".join(map(str, value["active"])) + "'", len(value["active"]), value["isk"] - getIskLossForCorp(key), value["sumonkills"]))
+        cur.execute(
+            'INSERT INTO "zwhAggregate" ("date", "corporation", "kills", "losses", "iskWon", "iskLost", "active", "numActive", sumOnKills")' +
+            ' VALUES (%i, %s, %i, %i, %f, %f, %s, %i, %i)' % (
+            int(date),
+            "'" + key.replace("'", "''") + "'",
+            value["kills"],
+            getLossesForCorp(key),
+            value["iskWon"],
+            getIskLossForCorp(key),
+            "'" + ",".join(map(str, value["active"])) + "'",
+            len(value["active"]),
+            value["sumOnKills"]))
     conn.commit()
 
 
@@ -160,7 +188,7 @@ for date in DATES:
             if killmailCREST != [] and (reJMail.match(killmailCREST["solarSystem"]["name"]) or killmailCREST["solarSystem"]["name"] == "J1226-0"):
                 updateMasterDict(killmailCREST, getZKB(killmailCREST["killID_str"]))
                 jMailCounter += 1
-            elif not killmailCREST: # 20160824 has the problematic first Keepstar kill that does not appear on CREST, this (and the above killmailCREST != []) is a temporary fix..
+            elif not killmailCREST:  # 20160824 has the problematic first Keepstar kill that does not appear on CREST, this (and the above killmailCREST != []) is a temporary fix..
                 print("[] error...")
             counter += 1
 
@@ -170,6 +198,3 @@ for date in DATES:
     updateDB(cur, date)
 
 conn.close()
-
-
-
